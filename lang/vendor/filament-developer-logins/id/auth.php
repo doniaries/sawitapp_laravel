1a749944711ea740e10023d0052cc4068e61862d
Doni Aries <doniaries@gmail.com>
2026-04-25 16:17:40 +0700

    add:bahasa indo

---
+++ lang/vendor/filament-developer-logins/id/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-as' => 'Masuk sebagai',
+    'switch-to' => 'Beralih ke',
+
+    'messages' => [
+        'failed' => 'Gagal masuk, periksa apakah pengguna diizinkan untuk mengakses panel ini.',
+        'failed_not_found' => 'Gagal masuk, pengguna tidak ditemukan.',
+    ],
+];
