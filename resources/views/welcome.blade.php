7e020846b7e09da00a73d60dad235513a93f51d7
Doni Aries <doniaries@gmail.com>
2026-04-28 19:34:15 +0700

    fgfgf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Success Mandiri - Perusahaan Terdaftar</title>
+    <title>Aplikasi Transaksi Penjualan Sawit</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
